e85321139890424aa6030c02fed38f67deb1e108
Alen Vuletić <alenvuletic@gmail.com>
2024-12-28 18:14:41 +0100

    Added utilities layer. Updated google fonts.

--- wp-content/themes/laqonto/functions.php
+++ wp-content/themes/laqonto/functions.php
@@ -6,8 +6,8 @@
 function laqonto_scripts_styles() {
   wp_enqueue_style( 'style-settings', get_template_directory_uri() . '/css/settings.css' );
   wp_enqueue_style( 'style-preflight', get_template_directory_uri() . '/css/preflight.css' );
-  wp_enqueue_style( 'style-google-fonts', '//fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap' );
+  wp_enqueue_style( 'style-google-fonts', '//fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap', [], null );
   wp_enqueue_style( 'style', get_stylesheet_uri() );
-  wp_enqueue_script( 'script-header', get_template_directory_uri() . '/js/header.js', array(), false, true );
+  wp_enqueue_script( 'script-header', get_template_directory_uri() . '/js/header.js', [], false, true );
 }
 add_action('wp_enqueue_scripts', 'laqonto_scripts_styles');
