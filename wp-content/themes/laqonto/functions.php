26f13c95231adcde34d7ff0926066a607dfa8352
Alen Vuletić <alenvuletic@gmail.com>
2025-01-09 08:24:44 +0100

    Updated footer component.

--- wp-content/themes/laqonto/functions.php
+++ wp-content/themes/laqonto/functions.php
@@ -4,10 +4,17 @@
  * Enqueue scripts and styles.
  */
 function laqonto_scripts_styles() {
-  wp_enqueue_style( 'style-settings', get_template_directory_uri() . '/css/settings.css' );
-  wp_enqueue_style( 'style-preflight', get_template_directory_uri() . '/css/preflight.css' );
-  wp_enqueue_style( 'style-google-fonts', '//fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap', [], null );
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
-  wp_enqueue_script( 'script-header', get_template_directory_uri() . '/js/header.js', [], false, true );
+  wp_enqueue_style( "style-settings", get_template_directory_uri() . "/css/settings.css" );
+  wp_enqueue_style( "style-preflight", get_template_directory_uri() . "/css/preflight.css" );
+  wp_enqueue_style( "style-google-fonts", "//fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap", [], null );
+  wp_enqueue_style( "style", get_stylesheet_uri() );
+  wp_enqueue_script( "script-header", get_template_directory_uri() . "/js/header.js", [], false, true );
 }
-add_action('wp_enqueue_scripts', 'laqonto_scripts_styles');
+add_action( "wp_enqueue_scripts", "laqonto_scripts_styles" );
+
+/**
+ * Get icon
+ */
+function get_icon( $name ) {
+  return file_get_contents( "wp-content/themes/laqonto/icons/$name.svg" );
+}
