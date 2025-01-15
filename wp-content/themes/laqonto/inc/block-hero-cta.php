a81bffe7f17a4239212a08493a0d279f52da1437
Alen Vuletić <alenvuletic@gmail.com>
2025-01-15 07:58:15 +0100

    Added block hero CTA.

---
+++ wp-content/themes/laqonto/inc/block-hero-cta.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class="hero-cta">
+    <div class="hero-cta__content">
+      <p class="hero-cta__title">Zatraži ponudu</p>
+      <a href="#" class="button button--primary button--small button--rounded">Ispuni upitnik</a>
+    </div>
+  </div>
+</div>
