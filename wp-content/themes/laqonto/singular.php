224c4b7a404e13ecddf1ae10ca4f48137a002768
Alen Vuletić <alenvuletic@gmail.com>
2025-01-16 07:17:11 +0100

    Finished singular template.

--- wp-content/themes/laqonto/singular.php
+++ wp-content/themes/laqonto/singular.php
@@ -4,7 +4,12 @@
   <header class="entry-header">
     <div class="grid">
       <div class="col-span-12 lg:col-span-4 mb-7 lg:mb-0 lg:mt-12">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="button button--secondary button--rounded button--small">Naslovnica</a>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="button button--secondary button--rounded button--small button--has-icon">
+          <span class="button__icon">
+            <?php include get_theme_file_path( '/icons/arrow-left.svg' ); ?>
+          </span>
+          <span>Naslovnica</span>
+        </a>
       </div>
       <div class="col-span-12 lg:col-span-6">
         <h1 class="entry-title"><?php the_title(); ?></h1>
