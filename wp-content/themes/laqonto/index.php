2d7ec864af098441eff08e1059e6e4d8f3fb6fab
Alen Vuletić <alenvuletic@gmail.com>
2025-02-13 18:45:36 +0100

    Added `hero` component.

---
+++ wp-content/themes/laqonto/index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="hero">
+    <?php echo wp_get_attachment_image( 11, array( "1328", "720" ), false, array( "class" => "hero__background" ) ); ?>
+    <div class="hero__content">
+      <p>Moderno knjigovodstvo, prilagođeno tvom poslovanju.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="hero text-white">
+    <?php echo wp_get_attachment_image( 11, array( "1328", "720" ), false, array( "class" => "hero__background" ) ); ?>
+    <div class="hero__content">
+      <p>Digitalno knjigovodstvo koje ide u korak s tvojim biznisom.</p>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
