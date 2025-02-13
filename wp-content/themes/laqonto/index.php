f387373254228049b96fb3a68cdb8e10aa4eb790
Alen Vuletić <alenvuletic@gmail.com>
2025-02-13 19:30:41 +0100

    Finished hero sections.

--- wp-content/themes/laqonto/index.php
+++ wp-content/themes/laqonto/index.php
@@ -1,6 +1,19 @@
 <?php get_header(); ?>
 
 <div class="container">
+  <div class="hero-subheader">
+    <div class="hero-subheader__inner text-white">
+      <h1 class="hero-subheader__heading">Jednostavno knjigovodstvo za novu generaciju poduzetnika.</h1>
+    </div>
+    <div class="hero-subheader__arrow">
+      <svg width="32" height="77" viewBox="0 0 32 77" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 62C5 62 8 62.7222 10 65.6111C11.6 67.9222 14.6667 72.8333 16 75C17.3333 72.8333 20.4 67.9222 22 65.6111C23.9375 62.8125 27 62 32 62" stroke="#FC663D" stroke-width="4" stroke-linejoin="bevel"/><path d="M16 60L16 0" stroke="#FC663D" stroke-width="4"/></svg>
+    </div>
+    <?php echo wp_get_attachment_image( 11, array( "1328", "720" ), false, array( "class" => "hero-subheader__background" ) ); ?>
+  </div>
+</div>
+
+
+<div class="container">
   <div class="hero">
     <?php echo wp_get_attachment_image( 11, array( "1328", "720" ), false, array( "class" => "hero__background" ) ); ?>
     <div class="hero__content">
