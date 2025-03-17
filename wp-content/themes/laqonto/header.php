694b9ef7495b02ce34f412772aec424fabf1ecb9
Alen Vuletić <alenvuletic@gmail.com>
2025-03-17 08:47:37 +0100

    Updated `<title` in header.

--- wp-content/themes/laqonto/header.php
+++ wp-content/themes/laqonto/header.php
@@ -14,7 +14,9 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <title>
+      <?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?>
+    </title>
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
     <?php wp_head(); ?>
