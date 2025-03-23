600fe1c65fe2fc0f9a053e7cc031d3b404b7cff1
Alen Vuletić <alenvuletic@gmail.com>
2025-03-23 17:13:27 +0100

    Updated `documents.php`.

--- wp-content/themes/laqonto/page-templates/documents.php
+++ wp-content/themes/laqonto/page-templates/documents.php
@@ -29,20 +29,20 @@
     <main>
       <div class="documents-blocks-section">
         <section class="documents-block">
-          <h2 class="documents-block__title">Punomoći</h2>
+          <h2 class="documents-block__title"><?php _e("Punomoći", "laqonto"); ?></h2>
           <ul>
             <li class="documents-block-item">
               <div class="documents-block-item__main">
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Dodjela punomoći e-RGFI</p>
+                <p><?php _e("Dodjela punomoći e-RGFI", "laqonto"); ?></p>
               </div>
-              <a href="https://laqonto.com/wp-content/uploads/2025/03/Dodjela-punomoci-e-RGFI.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/punomoc-e-rgfi.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -50,13 +50,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Uputa dodjele punomoći e-RGFI (putem e-Građana)</p>
+                <p><?php _e("Uputa dodjele punomoći e-RGFI (putem e-Građana)", "laqonto"); ?></p>
               </div>
-              <a href="https://laqonto.com/wp-content/uploads/2025/03/Uputa-dodjele-punomoci-e-RGFI-putem-e-Gradana.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/punomoc-e-rgfi-egradani.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -64,13 +64,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Pristupni list e-mirovinsko</p>
+                <p><?php _e("Pristupni list e-mirovinsko", "laqonto"); ?></p>
               </div>
-              <a href="https://laqonto.com/wp-content/uploads/2025/03/Pristupni-list-e-mirovinsko.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/pristupni-list-e-mirovinsko.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -78,13 +78,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Pristupni list e-zdravstveno</p>
+                <p><?php _e("Pristupni list e-zdravstveno", "laqonto"); ?></p>
               </div>
-              <a href="#" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/e-zdravstveno.docx" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
           </ul>
@@ -97,13 +97,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Izdavanje certifikata</p>
+                <p><?php _e("Izdavanje certifikata", "laqonto"); ?></p>
               </div>
               <a href="https://www.fina.hr/finadigicert/dokumentacija-za-izdavanje-certifikata#poslovni" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -111,13 +111,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>e-građani</p>
+                <p><?php _e("e-građani", "laqonto"); ?></p>
               </div>
               <a href="https://gov.hr/" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -125,13 +125,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>e-potpis poduzeća</p>
+                <p><?php _e("e-potpis poduzeća", "laqonto"); ?></p>
               </div>
               <a href="https://eposlovanje.fina.hr/WebEPotpis" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -139,13 +139,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>e-potpis fizičke osobe</p>
+                <p><?php _e("e-potpis fizičke osobe", "laqonto"); ?></p>
               </div>
               <a href="https://web.epotpis.rdd.hr/" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
           </ul>
@@ -158,13 +158,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Putni nalog 2025</p>
+                <p><?php _e("Putni nalog 2025", "laqonto"); ?></p>
               </div>
-              <a href="#" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/putni-nalog.xlsx" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -172,13 +172,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Obračun loko vožnje</p>
+                <p><?php _e("Obračun loko vožnje", "laqonto"); ?></p>
               </div>
-              <a href="#" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/loko-voznja.xlsx" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -186,13 +186,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Tiskanice HZZO</p>
+                <p><?php _e("Tiskanice HZZO", "laqonto"); ?></p>
               </div>
               <a href="http://www.hzzo.hr/tiskanice" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -200,13 +200,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Tiskanice HZMO</p>
+                <p><?php _e("Tiskanice HZMO", "laqonto"); ?></p>
               </div>
               <a href="http://www.mirovinsko.hr/default.aspx?id=752" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -214,13 +214,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Obračun naknade za odvojeni život</p>
+                <p><?php _e("Obračun naknade za odvojeni život", "laqonto"); ?></p>
               </div>
-              <a href="https://laqonto.com/wp-content/uploads/2025/03/obracun-naknade-za-odvojeni-zivot.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/obracun-naknade-za-odvojeni-zivot-1.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -228,13 +228,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Obračun pomorskog dodatka</p>
+                <p><?php _e("Obračun pomorskog dodatka", "laqonto"); ?></p>
               </div>
-              <a href="https://laqonto.com/wp-content/uploads/2025/03/obracun-pomorskog-dodatka.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/obracun-pomorskog-dodatka-1.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
           </ul>
@@ -247,13 +247,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Prijava činjenica bitnih za oporezivanja</p>
+                <p><?php _e("Prijava činjenica bitnih za oporezivanja", "laqonto"); ?></p>
               </div>
-              <a href="https://laqonto.com/wp-content/uploads/2025/03/Obrazac_Prijava_cinjenica_bitnih_za_oporezivanje_NN4519.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/obrazac-prijava-cinjenica-bitnih-za-oporezivanje-nn4519.pdf" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -261,13 +261,13 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Interni akt</p>
+                <p><?php _e("Interni akt", "laqonto"); ?></p>
               </div>
-              <a href="#" class="button button--secondary button--medium button--has-icon gap-3">
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/interni-akt.docx" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
             <li class="documents-block-item">
@@ -275,13 +275,27 @@
                 <span class="documents-block-item__icon">
                   <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
                 </span>
-                <p>Izvadak iz sudskog registra</p>
+                <p><?php _e("Blagajnički maksimum", "laqonto"); ?></p>
+              </div>
+              <a href="https://laqonto.com/wp-content/uploads/2025/03/blagajnicki-maksimum.docx" class="button button--secondary button--medium button--has-icon gap-3">
+                <span class="button__icon">
+                  <?php include get_theme_file_path( '/icons/download.svg' ); ?>
+                </span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
+              </a>
+            </li>
+            <li class="documents-block-item">
+              <div class="documents-block-item__main">
+                <span class="documents-block-item__icon">
+                  <?php include get_theme_file_path( '/icons/file-text.svg' ); ?>
+                </span>
+                <p><?php _e("Izvadak iz sudskog registra", "laqonto"); ?></p>
               </div>
               <a href="https://sudreg.pravosudje.hr/registar/f?p=150:1" target="_blank" class="button button--secondary button--medium button--has-icon gap-3">
                 <span class="button__icon">
                   <?php include get_theme_file_path( '/icons/download.svg' ); ?>
                 </span>
-                <span>Preuzmi</span>
+                <span><?php _e("Preuzmi", "laqonto"); ?></span>
               </a>
             </li>
           </ul>
